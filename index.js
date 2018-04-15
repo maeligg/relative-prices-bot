c4180988d4e8b3d010d6f4911e8217882eac2157
Maël Brunet <maelbrunet@gmail.com>
2018-04-15 19:23:34 +0200

    Migrate hosting to heroku

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-const Twit = require('twit');
-const prices = require('./prices.json');
-const { createServer } = require('http');
-
-// Now cli requires an HTTP server to deploy the app
-const server = createServer(() => {});
-server.listen(3000);
+const Twit = require("twit");
+const prices = require("./prices.json");
 
 // Uncomment these lines if running locally (see readme for more details)
 // const config = require('./config.js');
@@ -14,7 +9,7 @@ const T = new Twit({
   consumer_key: process.env.consumer_key,
   consumer_secret: process.env.consumer_secret,
   access_token: process.env.access_token,
-  access_token_secret: process.env.access_token_secret,
+  access_token_secret: process.env.access_token_secret
 });
 
 let selectedItemA;
@@ -25,12 +20,13 @@ const pickRandomItem = () => {
   return keys[Math.floor(Math.random() * keys.length)];
 };
 
-const stringToNumber = stringNumber => parseFloat(stringNumber.replace(/,/g, ''));
+const stringToNumber = stringNumber =>
+  parseFloat(stringNumber.replace(/,/g, ""));
 
-const formatNumber = (number) => {
+const formatNumber = number => {
   const string = number.toString();
 
-  const formattedString = string.replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+  const formattedString = string.replace(/(\d)(?=(\d{3})+$)/g, "$1,");
 
   return formattedString;
 };
@@ -47,7 +43,8 @@ const comparePrices = () => {
   // We need to make sure the price different between the two items goes in the right direction and is big enough
   while (
     itemB === undefined ||
-    stringToNumber(prices[itemA].price) / stringToNumber(prices[itemB].price) < 2
+    stringToNumber(prices[itemA].price) / stringToNumber(prices[itemB].price) <
+      2
   ) {
     itemB = pickRandomItem();
   }
@@ -59,16 +56,21 @@ const comparePrices = () => {
 // Build and post the tweet
 const tweet = () => {
   comparePrices();
-  const ratio = formatNumber(Math.floor(stringToNumber(prices[selectedItemA].price) / stringToNumber(prices[selectedItemB].price)));
+  const ratio = formatNumber(
+    Math.floor(
+      stringToNumber(prices[selectedItemA].price) /
+        stringToNumber(prices[selectedItemB].price)
+    )
+  );
   const tweetText = `With ${selectedItemA}, you could buy ${ratio} ${
     prices[selectedItemB].plural
   }.`;
 
-  T.post('statuses/update', { status: tweetText }, (postErr, postData) => {
+  T.post("statuses/update", { status: tweetText }, (postErr, postData) => {
     if (postErr) {
-      console.log('error: ', postErr);
+      console.log("error: ", postErr);
     } else {
-      console.log('response: ', postData);
+      console.log("response: ", postData);
     }
   });
 };
